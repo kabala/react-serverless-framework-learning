ddca1a8af896f7ad301d4377d69beab99feb1a94
Carlos Santín <kabaldesign@gmail.com>
2021-08-09 15:47:26 -0500

    Added Auth files.

--- backend/scripts/config.js
+++ backend/scripts/config.js
@@ -1,16 +1,36 @@
-const fs = require('fs');
-const path = require('path');
+/**
+ * This script exports the serverless configuration function for the
+ * serverless-stack-output Serverless Framework plugin.
+ *
+ * @author Victor Santos Uceta
+ * @license Attribution-NonCommercial-NoDerivatives 4.0 International
+ */
+const fs = require('fs')
+const path = require('path')
 
-function handler(data, serverless, options) {
-    console.log('Received Data', data)
-    console.log('Received Options', options)
+/*  this handler will be called by the plugin 'serverless-stack-output' */
+function handler (data, serverless, options) {
+    /* log the incoming data */
+    console.log('Received data', data)
+    console.log('Received options', options)
 
+    /* build the configuration string in a .env file */
     let config = [
-        `REACT_APP_BUCKET_NAME=${data.exampleBucketUrl}`,
-        `VALUE=${2}`
-    ].join('\n');
-
-    fs.writeFileSync(path.join(__dirname, `../../.${options.stage}.env`), config);
+        `REACT_APP_AWS_REGION=${options.region}`,
+        `REACT_APP_ENVIRONMENT=${options.stage}`,
+        `REACT_APP_USER_POOL_ID=${data.UserPoolId}`,
+        `REACT_APP_ENDPOINT=${data.ServiceEndpoint}`,
+        `REACT_APP_IDENTITY_POOL_ID=${data.IdentityPoolId}`,
+        `REACT_APP_PICTURES_TABLE=${data.PicturesTableName}`,
+        `REACT_APP_PICTURES_BUCKET=${data.PicturesBucketName}`,
+        `REACT_APP_FRONT_END_BUCKET=${data.FrontEndBucketName}`,
+        `REACT_APP_USER_POOL_CLIENT_ID=${data.UserPoolClientId}`,
+        `REACT_APP_CLOUDFRONT_URL=https://${data.ProServerlessCloudFrontDistribution}`,
+        `REACT_APP_FRONT_END_S3_URL=http://${data.FrontEndBucketName}.s3.amazonaws.com`
+    ].join('\n')
+    /* write configuration to file */
+    // fs.writeFileSync(path.join(__dirname, `../../.env.${options.stage}`), config)
+    fs.writeFileSync(path.join(__dirname, `../../.env`), config)
 }
-
-module.exports = {handler}
+/* export the handler */
+module.exports = { handler }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import "antd/dist/antd.css"
 
 function App() {
   console.log(process.env)
