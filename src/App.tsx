c583b62526ecbb0af9057442995b65f459bdae3c
Carlos Santín <kabaldesign@gmail.com>
2021-08-09 19:51:52 -0500

    Added eslint and prettier config for TS.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'antd/dist/antd.css';
 
 function App() {
-  console.log(process.env)
-
   return (
     <div className="App">
       <header className="App-header">
