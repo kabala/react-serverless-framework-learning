ecb4aa4beaa9d47dfcfb553d480f1adbf9fd9089
Carlos Santín <kabaldesign@gmail.com>
2021-08-11 21:59:58 -0500

    Refactored SignIn Component.

--- src/components/auth/SignIn.tsx
+++ src/components/auth/SignIn.tsx
@@ -9,81 +9,45 @@ import React, { useState } from 'react';
 import { Form, Input, Button } from 'antd';
 import { UserOutlined, LockOutlined } from '@ant-design/icons';
 /* local imports */
-// import { setUsername } from 'store/actions/authActions';
+import { signIn, setUsername } from 'store/slices/authSlice';
 import switchAuthenticationForm from 'store/actions/global';
 import { useAppDispatch } from 'store/hooks';
 import {
   NAVIGATION_AUTHENTICATION_SIGN_UP,
   NAVIGATION_AUTHENTICATION_FORGOT_PASSWORD,
-  // NAVIGATION_AUTHENTICATION_CONFIRM_ACCOUNT,
+  NAVIGATION_AUTHENTICATION_CONFIRM_ACCOUNT,
 } from 'lib/types';
 
-// interface SignUpFormFields {
-//   userName: string;
-//   password: string;
-// }
+interface SignUpFormFields {
+  userName: string;
+  password: string;
+}
 
 export default function SignIn() {
-  const [form] = Form.useForm();
   const [isLoading, setIsLoading] = useState(false);
   const dispatch = useAppDispatch();
-
-  /**
-   * Helper function to display any error of sign in operation.
-   * @param  {string} error
-   * @param  {Object} values
-   */
-  // function evaluateSignInResult(error: string, values: SignUpFormFields) {
-  //   /* determine the error by evaluating true conditions. */
-  //   switch (true) {
-  //     /* the account is not yet confirmed, switch to confirm account form */
-  //     case error.includes('not confirmed'):
-  //       dispatch(setUsername(values.userName));
-  //       dispatch(switchAuthenticationForm(NAVIGATION_AUTHENTICATION_CONFIRM_ACCOUNT));
-  //       break;
-  //     /* there was another error, display error message in password field */
-  //
-  //     default:
-  //       form.setFields([
-  //         {
-  //           name: 'password',
-  //           value: values.password,
-  //           errors: [error],
-  //         },
-  //       ]);
-  //   }
-  // }
 
   /**
    * Handles the sign in submission
    * @param  {Event} e
    */
-  async function handleSubmit(e: Event): Promise<void> {
+  async function handleSubmit({ userName, password }: SignUpFormFields): Promise<void> {
     /* cancel the form submission, we want to login manually instead */
-    e.preventDefault();
+    console.log(userName, password);
+    // e.preventDefault();
     /* switch on loading spin on parent component */
     setIsLoading(true);
-    /* validate fields */
-
-    // await form.validateFields(async (err, values) => {
-    //   if (!err) {
-    //     /* try to sign in asynchronously, if success, undefined is returned, otherwise
-    //             a string message is returned. */
-    //     const error = await signIn(values.userName, values.password);
-    //     /* if there was an error, call helper function */
-    //     if (error) {
-    //       evaluateSignInResult(error, values);
-    //     }
-    //   }
-    //   /* disable loading UI */
-    //   setIsLoading(false);
-    // });
     try {
-      const formValidationData = await form.validateFields();
-      console.log(formValidationData);
-    } catch (errorInfo) {
-      console.log(errorInfo);
+      await signIn(userName, password);
+    } catch (error) {
+      if (error.includes('not confirmed')) {
+        dispatch(setUsername(userName));
+        dispatch(switchAuthenticationForm(NAVIGATION_AUTHENTICATION_CONFIRM_ACCOUNT));
+      }
+
+      console.log(error);
     }
+    setIsLoading(false);
   }
   /**
    * Renders the sign in form component.
@@ -93,10 +57,18 @@ export default function SignIn() {
   // const { getFieldDecorator } = this.props.form;
   return (
     <Form onFinish={handleSubmit}>
-      <Form.Item name="userName" label="User">
+      <Form.Item
+        name="userName"
+        label="User"
+        rules={[{ required: true, message: 'Insert your username.' }]}
+      >
         <Input prefix={<UserOutlined />} placeholder="Username" />
       </Form.Item>
-      <Form.Item name="password" label="Password">
+      <Form.Item
+        name="password"
+        label="Password"
+        rules={[{ required: true, message: 'Insert your password.' }]}
+      >
         <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
       </Form.Item>
       <Form.Item>
@@ -129,21 +101,3 @@ export default function SignIn() {
     </Form>
   );
 }
-
-/* redux map state to properties */
-/*
-const mapStateToProps = (state, ownProps) => {
-  return {
-    switchLoading: ownProps.switchLoading,
-    username: state.authentication.username,
-  };
-};
- */
-/* redux map dispatch functions to properties */
-/*
-const mapDispatchToProps = {
-  signIn,
-  setUsername,
-  switchAuthenticationForm,
-};
-*/
